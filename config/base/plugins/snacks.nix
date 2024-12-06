ecd32bdee3f49037802ccb8a3c77d1361255d74f
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-07 00:21:30 +0100

    added snacks

---
+++ config/base/plugins/snacks.nix
@@ -0,0 +1,10 @@
+{
+    plugins.snacks = {
+        enable = true;
+        settings = {
+            notifier.enable = true;
+            quickfile.enable = true;
+            rename.enable = true;
+        };
+    };
+}
