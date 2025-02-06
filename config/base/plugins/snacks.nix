ef01df7a37943553009ec0d3e1c0eac033197526
anders130 <93037023+anders130@users.noreply.github.com>
2025-02-06 09:47:44 +0100

    fix notifier position and timeout

--- config/base/plugins/snacks.nix
+++ config/base/plugins/snacks.nix
@@ -2,7 +2,11 @@
     plugins.snacks = {
         enable = true;
         settings = {
-            notifier.enable = true;
+            notifier = {
+                enable = true;
+                top_down = false;
+                timeout = 2000;
+            };
             quickfile.enable = true;
             rename.enable = true;
         };
