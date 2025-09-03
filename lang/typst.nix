b9c6b1505166b971632c94827ea4ee0fe81141cb
anders130 <93037023+anders130@users.noreply.github.com>
2025-09-03 09:16:47 +0200

    add typst support

---
+++ lang/typst.nix
@@ -0,0 +1,14 @@
+{
+    plugins = {
+        typst-vim.enable = true;
+        lsp.servers.tinymist = {
+            enable = true;
+            settings.formatterMode = "typstyle";
+        };
+        web-devicons.customIcons.typ = {
+            icon = "";
+            color = "#5acede";
+            name = "typst";
+        };
+    };
+}
