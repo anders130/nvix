3e4f43ba31903e345ef01847acc97675301b3347
anders130 <93037023+anders130@users.noreply.github.com>
2025-09-23 21:50:29 +0200

    add lilypond support

---
+++ lang/lilypond.nix
@@ -0,0 +1,10 @@
+{
+    plugins = {
+        lilypond-suite.enable = true;
+        web-devicons.customIcons.ly = {
+            icon = "󰴈";
+            color = "#97CE86";
+            name = "lilypond";
+        };
+    };
+}
