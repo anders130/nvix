1181e22095d8f5fc75370fbd8744c5079177313f
anders130 <93037023+anders130@users.noreply.github.com>
2025-12-21 17:01:09 +0100

    fix neo-tree git status

--- config/base/plugins/neo-tree.nix
+++ config/base/plugins/neo-tree.nix
@@ -1,6 +1,14 @@
-{
+{pkgs, ...}: {
     plugins.neo-tree = {
         enable = true;
+        package = pkgs.vimPlugins.neo-tree-nvim.overrideAttrs (old: {
+            src = pkgs.fetchFromGitHub {
+                owner = "nvim-neo-tree";
+                repo = "neo-tree.nvim";
+                rev = "b0b73273b4f5a1f4b4aac8accd6e0c3b4b5a6967";
+                sha256 = "sha256-PRhip7vEFDLh5zei3s/bS0AysbtT9iE7yYNFkIEqct8=";
+            };
+        });
         settings = {
             usePopupsForInput = false;
             buffers.followCurrentFile = {
