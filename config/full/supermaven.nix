6c344162585ee800bb14de58e74429b344ca4633
anders130 <93037023+anders130@users.noreply.github.com>
2026-03-22 17:30:54 +0100

    migrate to nixvim supermaven integration

--- config/full/supermaven.nix
+++ config/full/supermaven.nix
@@ -1,14 +1,13 @@
-{pkgs, ...}: {
-    extraPlugins = [pkgs.vimPlugins.supermaven-nvim];
-
-    extraConfigLua = /*lua*/''
-        require("supermaven-nvim").setup {
-            disable_inline_completion = false,
+{
+    plugins.supermaven = {
+        enable = true;
+        settings = {
+            disable_inline_completion = false;
             keymaps = {
-                accept_suggestion = "<S-TAB>",
-                clear_suggestion = "<C-S-j>",
-                accept_word = "<C-j>",
-            }
-        }
-    '';
+                accept_suggestion = "<S-TAB>";
+                clear_suggestion = "<C-S-j>";
+                accept_word = "<C-j>";
+            };
+        };
+    };
 }
