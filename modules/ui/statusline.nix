ec01bdb857ae47b9fa3f6c30193e8872dcfc26d2
anders130 <93037023+anders130@users.noreply.github.com>
2026-05-05 21:38:19 +0200

    feat(statusline): show relative path in lualine_c

--- modules/ui/statusline.nix
+++ modules/ui/statusline.nix
@@ -2,6 +2,13 @@
     flake.modules.ui.statusline = {
         plugins.lualine = {
             enable = true;
+            settings.sections.lualine_c = [
+                {
+                    __unkeyed-1 = "filename";
+                    path = 1;
+                    shorting_target = 40;
+                }
+            ];
             settings.options = {
                 section_separators = {
                     left = "";
